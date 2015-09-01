17ab04b282ccb987175e96099ae45670c91a26a8
Hartmut Bischoff <topofocus@gmail.com>
2015-09-01 06:15:35 +0200

    Some Enhancement to Examples

--- examples/books.rb
+++ examples/books.rb
@@ -1,15 +1,24 @@
+=begin
+Books Example
+
+There are several Books. And there is a Table with Keywords. These are our Vertex-Classes
+
+The Keywords are associated to the books, this is realized via an Edge »has_content«
+
+This Example demonstrates how to build a query by using OrientSupport::OrientQuery
+=end
  class BooksExample
 
     def initialize db, rebuild: true
       if rebuild
       db.delete_class :book
-      db.delete_class :word
+      db.delete_class :keyword
       db.delete_class :has_content
       db.create_vertex_class :book
-      ActiveOrient::Model::Book.create_property( :title, type: :string, index: :unique )
-      db.create_vertex_class :word
-      ActiveOrient::Model::Word.create_property( :item , type: :string, index: :unique )
+      db.create_vertex_class :keyword
       db.create_edge_class :has_content
+      ActiveOrient::Model::Keyword.create_property( :item , type: :string, index: :unique )
+      ActiveOrient::Model::Book.create_property( :title, type: :string, index: :unique )
       end
     end
 
@@ -17,7 +26,7 @@
       def read_samples
 	fill_database = ->(sentence, this_book ) do 
 	  sentence.split(' ').each do |x| 
-	    this_word = Word.update_or_create where: { item: x }
+	    this_word = Keyword.update_or_create where: { item: x }
 	    this_edge = HC.create_edge from: this_book, to: this_word  if this_word.present?
 	  end 
 	end
@@ -31,7 +40,7 @@
       end
   
     def display_books_with *desired_words
-      query = OrientSupport::OrientQuery.new  from: Word, projection: "expand(in('HasContent'))"
+      query = OrientSupport::OrientQuery.new  from: Keyword, projection: "expand(in('HasContent'))"
       q =  OrientSupport::OrientQuery.new projection: 'expand( $z )'
 
       intersects = Array.new
@@ -43,24 +52,26 @@
       q.let   "$z = Intersect(#{intersects.join(', ')}) "
       puts "generated Query:"
       puts q.to_s
-      result = Word.query_database  q, set_from: false
+      result = Keyword.query_database  q, set_from: false
       puts "found books: "
       puts result.map( &:title ).join("; ") 
       puts " -- None -- " if result.empty?
     end
  end
 
+if $0 == __FILE__
 
 require '../config/boot'
 
-    ActiveOrient::OrientDB.default_server= { user: 'hctw', password: 'hc' }
+    ActiveOrient::OrientDB.default_server = { user: 'hctw', password: 'hc' }
+    ActiveOrient::OrientDB.logger.level = Logger::WARN
     r = ActiveOrient::OrientDB.new database: 'BookTest'
     b= BooksExample.new r, rebuild:  true 
       
     Book = r.open_class :book
-    Word = r.open_class :word
+    Keyword = r.open_class :keyword
     HC = r.open_class :has_content
     
-    b.read_samples if Word.count.zero?
+    b.read_samples if Keyword.count.zero?
     b.display_books_with 'Land', 'Quartal'
-
+end
--- examples/streets.rb
+++ examples/streets.rb
@@ -1,3 +1,17 @@
+=begin
+Streets Example
+
+
+We load german cities from wikipedia and parse the document for cities and countries(states).
+Further we load a collection of popular streetnames from a web-side called »strassen-in-deutschland«
+
+We define three vertices, State, City and Street.
+These are filled with the data from the web-sides
+
+Then we connect the cities through streets just by creating edges.
+At last we print the connected cities.
+
+=end
 module DataImport
   def read_german_street_names
     doc = Nokogiri::HTML(open('http://www.strassen-in-deutschland.de/die-haeufigsten-strassennamen-in-deutschland.html'))
@@ -32,44 +46,49 @@ end # module
    include  DataImport
     def initialize db, rebuild: true
       if rebuild
+	ActiveOrient::OrientDB.logger.progname = "StreetsExample#Initialize"
 	db.delete_class :state
-	db.create_vertex_class :state 
-	ActiveOrient::Model::State.create_property( :name, type: :string, index: :unique )
 	db.delete_class :city
+	db.delete_class :street
+	db.delete_class :connects
+	db.create_vertex_class :state 
 	db.create_vertex_class :city
+	db.create_vertex_class :street
+	db.create_edge_class :connects
+	ActiveOrient::Model::State.create_property( :name, type: :string, index: :unique )
 	ActiveOrient::Model::City.create_properties(  { name: { type: :string },
 			    state: { type: :link, :linked_class => 'State' } } 
 			      ) do
 				{ citi_idx: :unique } 
 			      end
-	db.delete_class :street
-	db.create_vertex_class :street
-	ActiveOrient::Model::Street.create_property( :name , type: :string, index: :notunique )
-	db.delete_class :connects
-	db .create_edge_class :connects
-	ActiveOrient::Model::Connects.create_property( :distance, type: :integer, index: :notunique )
+	ActiveOrient::Model::Street.create_property :name , type: :string, index: :notunique 
+	ActiveOrient::Model::Connects.create_property :distance, type: :integer, index: :notunique 
+	ActiveOrient::OrientDB.logger.info { "Vertex- and Edge-Classes rebuilded" }
       end
     end
 
 
-      def read_from_web
-	read_german_cities_and_states_fom_wikipedia.each do |city,state| 
-	  state =  State.update_or_create( where: { name: state }).first
-	  city = City.create name: city, state: state.link
-	end
-
-	cities_rids =  City.all.map &:link
-	read_german_street_names.each_with_index do |street, i|
-	  street_record =  Street.create name: street
-	  count = i
-	  cities =  Array.new
-	  while  count < cities_rids.size && cities.size < 5 do
-	    cities << cities_rids[count] 
-	    count =  count + i 
-	  end
-	  C.create_edge :from => street_record,  :to => cities
-	end
+    def read_from_web
+      ActiveOrient::OrientDB.logger.progname = "StreetsExample#ReadFromWeb"
+      read_german_cities_and_states_fom_wikipedia.each do |city,state| 
+	state =  State.update_or_create( where: { name: state }).first
+	City.create name: city, state: state.link
       end
+      ActiveOrient::OrientDB.logger.info { "#{City.count} Cities imported from Wikipedia " }
+
+      cities_rids =  City.all.map &:link
+      read_german_street_names.each_with_index do |street, i|
+	street_record =  Street.create name: street
+	count = i
+	cities =  Array.new
+	while  count < cities_rids.size && cities.size < 5 do
+	  cities << cities_rids[count] 
+	  count =  count + i 
+	end
+	C.create_edge :from => street_record,  :to => cities
+      end
+      ActiveOrient::OrientDB.logger.info { "#{C.count} Edges between Streets and Cities created " }
+    end
   
     def display_streets_per_state
       State.all.each do |state|
@@ -88,6 +107,7 @@ end # module
     end
  end
 
+if $0 == __FILE__
 
 require '../config/boot'
 require 'open-uri'
@@ -95,6 +115,7 @@ require 'nokogiri'
 
     ActiveOrient::OrientDB.default_server= { user: 'hctw', password: 'hc' }
     r = ActiveOrient::OrientDB.new database: 'StreetTest'
+    ActiveOrient::OrientDB.logger.level = Logger::INFO
     s= StreetExample.new r, rebuild:  true 
       
     City = r.open_class :city
@@ -105,3 +126,4 @@ require 'nokogiri'
     s.read_from_web if City.count.zero?
     s.display_streets_per_state
 
+end
