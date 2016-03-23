a261180787caf5c71d354096fc8eb747c6de0494
StefanoMartin <mishtir@gmail.com>
2016-03-23 13:14:43 +0100

    Modified eample

--- examples/streets.rb
+++ examples/streets.rb
@@ -15,15 +15,15 @@ At last we print the connected cities.
 module DataImport
   def read_german_street_names
     doc = Nokogiri::HTML(open('http://www.strassen-in-deutschland.de/die-haeufigsten-strassennamen-in-deutschland.html'))
-    strassen = doc.css('#strassen-in-deutschland_main a' ) # identified via css-inspector in browser
+    strassen = doc.css("td[data-header='Straßenname: '] a") # identified via css-inspector in browser
     # search for the css and include only links, then display the text-part
     strassen.children.map( &:to_s )[3..-1]  # omit the first three (strassen in deutschland, straßenverzeichnis, straßen)
   end
 
-  def read_german_cities_from_wikipedia 
-    # we extract <li> -elements and use the text until "(" 
+  def read_german_cities_from_wikipedia
+    # we extract <li> -elements and use the text until "("
     #doc.xpath("//li").at(80)
-    # => #<Nokogiri::XML::Element:0x1ba551c name="li" children=[#<Nokogiri::XML::Element:0x1ba533c name="a" attributes=[#<Nokogiri::XML::Attr:0x1ba52d8 name="href" value="/wiki/Angerm%C3%BCnde">, #<Nokogiri::XML::Attr:0x1ba52c4 name="title" value="Angermünde">] children=[#<Nokogiri::XML::Text:0x1ba4c84 "Angermünde">]>, #<Nokogiri::XML::Text:0x1ba4ae0 " (Brandenburg)">]> 
+    # => #<Nokogiri::XML::Element:0x1ba551c name="li" children=[#<Nokogiri::XML::Element:0x1ba533c name="a" attributes=[#<Nokogiri::XML::Attr:0x1ba52d8 name="href" value="/wiki/Angerm%C3%BCnde">, #<Nokogiri::XML::Attr:0x1ba52c4 name="title" value="Angermünde">] children=[#<Nokogiri::XML::Text:0x1ba4c84 "Angermünde">]>, #<Nokogiri::XML::Text:0x1ba4ae0 " (Brandenburg)">]>
 
     doc = Nokogiri::HTML(open('https://en.wikipedia.org/wiki/List_of_cities_and_towns_in_Germany'))
     doc.xpath("//li").map{|x| x.text[0 .. x.text.index('(')-2] if x.text.index('(').present? }.compact
@@ -32,12 +32,12 @@ module DataImport
   def read_german_cities_and_states_fom_wikipedia
     doc =
       Nokogiri::HTML(open('https://en.wikipedia.org/wiki/List_of_cities_and_towns_in_Germany'))
-    doc.xpath("//li").map do |x| 
-      if x.text.index('(').present? 
+    doc.xpath("//li").map do |x|
+      if x.text.index('(').present?
 	[ x.text[0 .. x.text.index('(')-2] , x.text[ x.text.index('(')+1 .. x.text.index(')')-1] ]
       end
     end.compact
-  end 
+  end
 
 end # module
 
@@ -51,18 +51,18 @@ end # module
 	db.delete_class :city
 	db.delete_class :street
 	db.delete_class :connects
-	db.create_vertex_class :state 
+	db.create_vertex_class :state
 	db.create_vertex_class :city
 	db.create_vertex_class :street
 	db.create_edge_class :connects
 	ActiveOrient::Model::State.create_property( :name, type: :string, index: :unique )
 	ActiveOrient::Model::City.create_properties(  { name: { type: :string },
-			    state: { type: :link, :linked_class => 'State' } } 
+			    state: { type: :link, :linked_class => 'State' } }
 			      ) do
-				{ citi_idx: :unique } 
+				{ citi_idx: :unique }
 			      end
-	ActiveOrient::Model::Street.create_property :name , type: :string, index: :notunique 
-	ActiveOrient::Model::Connects.create_property :distance, type: :integer, index: :notunique 
+	ActiveOrient::Model::Street.create_property :name , type: :string, index: :notunique
+	ActiveOrient::Model::Connects.create_property :distance, type: :integer, index: :notunique
 	ActiveOrient::OrientDB.logger.info { "Vertex- and Edge-Classes rebuilded" }
       end
     end
@@ -70,7 +70,7 @@ end # module
 
     def read_from_web
       ActiveOrient::OrientDB.logger.progname = "StreetsExample#ReadFromWeb"
-      read_german_cities_and_states_fom_wikipedia.each do |city,state| 
+      read_german_cities_and_states_fom_wikipedia.each do |city,state|
 	state =  State.update_or_create( where: { name: state }).first
 	City.create name: city, state: state.link
       end
@@ -82,14 +82,14 @@ end # module
 	count = i
 	cities =  Array.new
 	while  count < cities_rids.size && cities.size < 5 do
-	  cities << cities_rids[count] 
-	  count =  count + i 
+	  cities << cities_rids[count]
+	  count =  count + i
 	end
 	C.create_edge :from => street_record,  :to => cities
       end
       ActiveOrient::OrientDB.logger.info { "#{C.count} Edges between Streets and Cities created " }
     end
-  
+
     def display_streets_per_state
       State.all.each do |state|
 	streets= Street.all.map do |street |
@@ -116,13 +116,13 @@ require 'nokogiri'
     ActiveOrient::OrientDB.default_server= { user: 'hctw', password: 'hc' }
     r = ActiveOrient::OrientDB.new database: 'StreetTest'
     ActiveOrient::OrientDB.logger.level = Logger::INFO
-    s= StreetExample.new r, rebuild:  true 
-      
+    s= StreetExample.new r, rebuild:  true
+
     City = r.open_class :city
     State = r.open_class :state
     Street = r.open_class :street
     C = r.open_class :connects
-    
+
     s.read_from_web if City.count.zero?
     s.display_streets_per_state
 
